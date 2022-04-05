9f50f1a3579fbb27c429100d90e405d6db851768
mouhammeddbs0 <102580862+mouhammeddbs0@users.noreply.github.com>
2022-04-05 13:03:26 +0300

    Solve BackErorr 0

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const path = require('path');
-const mysql = require("mysql");
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 
 dotenv.config({path: './.env'});
 
 const app = express();
-
-const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
-});
 
 const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
@@ -26,13 +18,6 @@ app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 
-db.connect((error) => {
-    if (error) {
-        console.log(error)
-    } else {
-        console.log("MYSQL Connected...")
-    }
-})
 
 //Define Routes
 app.use('/', require('./routes/pages'));
--- db/db.js
+++ db/db.js
@@ -1,8 +1,17 @@
 const mysql = require("mysql");
 
-exports.db = mysql.createConnection({
+db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE
 });
+db.connect((error) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log("MYSQL Connected...")
+    }
+})
+
+module.exports = db;
