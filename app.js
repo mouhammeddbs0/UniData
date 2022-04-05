85cb050147390605f27c1bd8a61f52b028102136
mouhammeddbs0 <mouhammed672001@gmail.com>
2022-04-05 11:11:34 +0300

    ertt

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require('path');
-const mysql = require("mysql");
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 
@@ -8,12 +7,7 @@ dotenv.config({path: './.env'});
 
 const app = express();
 
-const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
-});
+const db = require('./db/db.js');
 
 const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
@@ -25,14 +19,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.set('view engine', 'ejs');
-
-db.connect((error) => {
-    if (error) {
-        console.log(error)
-    } else {
-        console.log("MYSQL Connected...")
-    }
-})
 
 //Define Routes
 app.use('/', require('./routes/pages'));
